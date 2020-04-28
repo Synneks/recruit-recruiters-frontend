9c3c6f9de2a7ebd67fbbfae96b54ca6189077bb3
Denis Eneotescu <Deniseneotescu@gmail.com>
2020-04-28 10:29:36 +0300

    added header elements

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatMenuTrigger } from '@angular/material/menu';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  @ViewChild('optionsMenu', { static: true }) trigger: MatMenuTrigger;
+  isLoggedIn = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  dropDown() {
+    this.trigger.openMenu();
   }
-
 }
