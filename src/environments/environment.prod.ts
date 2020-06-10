3bee5f38fd07f200cd15b4bcc576884f902c8843
Denis Eneotescu <Deniseneotescu@gmail.com>
2020-06-10 18:00:40 +0300

    make it best

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  crawlerUrl: 'https://recruit-recruiters-scraper.herokuapp.com/',
+  firebaseSignUpUrl: 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDz3mJHsYThaJFZbPFIeTH3yhTVL5KOEbM',
+  firebaseLoginUrl: 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyDz3mJHsYThaJFZbPFIeTH3yhTVL5KOEbM',
+  firebaseDbUrl: 'https://recruit-recruiters.firebaseio.com/',
+  firebaseUsersUrl: 'https://recruit-recruiters.firebaseio.com/users/',
 };
